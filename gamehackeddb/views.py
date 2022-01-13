51b9c0f56d02edfca7ba649b693a44cdf0947020
mwkresolve <mwkresolve.gmail.com>
2022-01-12 21:39:06 -0300

    hacked database add

--- gamehackeddb/views.py
+++ gamehackeddb/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from controller.models import HackedDatabase
+
+
+def dbview(request):
+    ips_user = HackedDatabase.objects.filter(userid=request.user).values()
+    return render(request, 'hackeddb.html', {'ipshacked': ips_user})
